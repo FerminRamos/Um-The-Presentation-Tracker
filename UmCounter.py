ad658d35134e2cbd6cd85489d9c973fa7e4b6345
Fermin Ramos <88788004+FerminRamos@users.noreply.github.com>
2021-08-19 21:53:32 -0600

    Added Comments
    There were no comments for giving an explanation. I added a few to give a brief explanation.

--- UmCounter.py
+++ UmCounter.py
@@ -1,8 +1,10 @@
 um_counter = 0
 like_counter = 0
 
+# Text-to-Speech File Name
 text_file = input("File Name: ")
 
+# Open File + Read
 with open("%s" % text_file) as monologue:
     monologue = monologue.readlines()
 
@@ -10,6 +12,7 @@ with open("%s" % text_file) as monologue:
 for words in monologue:
     list_words = words.split()
 
+# Count how many "um" & "like" words
 for word in list_words:
     if word == "um":
         print(word)
@@ -18,5 +21,6 @@ for word in list_words:
         print(word)
         like_counter += 1
 
+# Print total count
 print("UM counter =", um_counter)
 print("LIKE counter =", like_counter)
